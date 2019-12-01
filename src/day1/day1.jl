1c1a54aa07606c903bec26226200458a1bf5dbfd
Gauthier Soleilhac <gsoleilhac@gmail.com>
2019-12-02 00:07:59 +0100

    edit day1

--- src/day1/day1.jl
+++ src/day1/day1.jl
@@ -1,19 +1,14 @@
 module day1
 
 using DelimitedFiles
-
 const input = vec(readdlm(joinpath(@__DIR__, "input.txt"), Int))
 
 fuelRequired(x) = x ÷ 3 - 2
 part1(input) = sum(fuelRequired, input)
 
 function fuelRequired2(x)
-    fuel = totalFuel = fuelRequired(x)
-    while fuel > 0
-        fuel = max(0, fuelRequired(fuel))
-        totalFuel += fuel
-    end
-    totalFuel
+    fuel = fuelRequired(x)
+    fuel > 0 ? fuel + fuelRequired2(fuel) : 0
 end
 part2(input) = sum(fuelRequired2, input)
 
