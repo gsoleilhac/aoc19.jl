696888d2b41ec23fcdce8015d9734e93b07b0458
Gauthier Soleilhac <gsoleilhac@gmail.com>
2019-12-09 12:36:08 +0100

    fix day9part2 and timings

--- src/day9/day9.jl
+++ src/day9/day9.jl
@@ -8,7 +8,7 @@ const input = readdlm(joinpath(@__DIR__, "input.txt"), ',', Int) |> vec
 function part1(data, programInput = 1)
     output = Channel{Int}(Inf)
     input = Channel{Int}(Inf)
-    put!(input, 1)
+    put!(input, programInput)
     aoc19.run_program!(data, input, output)
     close(output)
     [o for o in output]
