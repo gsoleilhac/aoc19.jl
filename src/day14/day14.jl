27c6c71fe222b693c2dc3923e635c6ef4f12c920
Gauthier Soleilhac <gsoleilhac@gmail.com>
2019-12-14 18:31:35 +0100

    day14 cleanup

--- src/day14/day14.jl
+++ src/day14/day14.jl
@@ -1,17 +1,11 @@
 module day14
 
-import ..aoc19: run_program!
-using StaticArrays, Combinatorics, ProgressMeter
-
 readInput() = [split(line, r",|[=>]", keepempty=false) for line in readlines(joinpath(@__DIR__,"input.txt"))]
 struct Chemical
     type::Symbol
     qty::Int
 end
-Chemical(s::AbstractString) = begin 
-    x = split(s, ' ', keepempty=false)
-    Chemical(Symbol(x[2]), parse(Int, x[1]))
-end
+Chemical(s::AbstractString) = split(s, ' ', keepempty=false) |> x -> Chemical(Symbol(x[2]), parse(Int, x[1]))
 
 struct Reaction
     input::Vector{Chemical}
@@ -19,34 +13,31 @@ struct Reaction
 end
 Reaction(v::Vector{<:AbstractString}) = Reaction(Chemical.(v[1:end-1]), Chemical(v[end]))
 
-function produce!(n, chemical::Chemical, recipes::Dict{Symbol, Reaction}, products::Dict{Symbol, Int}, ore = Ref(0))
+function produce!(n::Int, chemical::Chemical, recipes::Dict{Symbol, Reaction}, products::Dict{Symbol, Int}, ore = Ref(0))
     if chemical.type == :ORE
         products[:ORE] += n
         ore[] += n
     else
         reaction = recipes[chemical.type]
-        m = ceil(Int, n / reaction.output.qty)
+        m = ceil(Int, n / reaction.output.qty) # how many times we need to apply the recipe to make at least n units of chemical
         for c in reaction.input
             if products[c.type] < m * c.qty
-                produce!(m * c.qty - products[c.type], c, recipes, products, ore)
+                missing_qty = m * c.qty - products[c.type]
+                produce!(missing_qty, c, recipes, products, ore)
             end
-            consume!(m, c, products)
+            products[c.type] -= m * c.qty #consume m times that product
         end
-        products[chemical.type] +=  m * reaction.output.qty
+        products[chemical.type] +=  m * reaction.output.qty # produce m times
     end
     ore[]
 end
 
-function consume!(m, c, products)
-    products[c.type] -= m * c.qty
-end
-
-function part1(data, n_fuel = 1)
+function part1(data)
     reactions = Reaction.(data)
     recipes = Dict(r.output.type => r for r in reactions)
     products = Dict(r => 0 for r in keys(recipes))
-    push!(products, :ORE => 0)
-    produce!(n_fuel, recipes[:FUEL].output, recipes, products)
+    products[:ORE] = 0
+    produce!(1, recipes[:FUEL].output, recipes, products)
 end
 
 function part2(data)
@@ -54,12 +45,11 @@ function part2(data)
     reactions = Reaction.(data)
     recipes = Dict(r.output.type => r for r in reactions)
     products = Dict(r => 0 for r in keys(recipes))
-    push!(products, :ORE => 0)
+    products[:ORE] = 0
 
-    min = max = 1
+    min, max = 1, 10
     while produce!(max, recipes[:FUEL].output, recipes, copy(products)) < max_ore
-        min = max
-        max *= 10
+        min, max = max, 10max
     end
     
     res = searchsortedfirst(min:max, max_ore ; 
